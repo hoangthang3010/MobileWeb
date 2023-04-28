d9cef9a23f7027c8b3eb15fbf70077ac8dd6f8ce
hoangthang3010 <thangminhhoang98@gmail.com>
2023-04-28 15:41:02 +0700

    update

--- src/components/dropproduct/DropProduct.js
+++ src/components/dropproduct/DropProduct.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
-import './DropProduct.scss'
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import "./DropProduct.scss";
+import { Link } from "react-router-dom";
 
 export default function Product() {
   const listmenu = [
@@ -14,32 +13,29 @@ export default function Product() {
           title: "iPhone",
           type: [
             {
-              ten: 'ip11',
+              ten: "ip11",
             },
             {
-              ten: 'ip12'
-            }
-          ]
+              ten: "ip12",
+            },
+          ],
         },
         {
           name: "samsung",
           title: "Samsung",
-          type: [
-          ]
+          type: [],
         },
         {
           name: "xiaomi",
           title: "Xiaomi",
-          type: [
-          ]
+          type: [],
         },
         {
           name: "oppo",
           title: "Oppo",
-          type: [
-          ]
-        }
-      ]
+          type: [],
+        },
+      ],
     },
     {
       title: "Đồng hồ thông minh",
@@ -48,27 +44,23 @@ export default function Product() {
         {
           name: "applewatch",
           title: "Apple Watch",
-          type: [
-          ]
+          type: [],
         },
         {
           name: "Samsung Watch",
           title: "Samsung Watch",
-          type: [
-          ]
+          type: [],
         },
         {
           name: "Xiaomi Watch",
           title: "Xiaomi Watch",
-          type: [
-          ]
+          type: [],
         },
         {
           name: "Oppo Watch",
-          type: [
-          ]
-        }
-      ]
+          type: [],
+        },
+      ],
     },
     {
       title: "Máy tính bảng",
@@ -76,23 +68,20 @@ export default function Product() {
       drop: [
         {
           name: "ipad",
-          title:"iPad",
-          type: [
-          ]
+          title: "iPad",
+          type: [],
         },
         {
           name: "samsung",
-          title:"Samsung",
-          type: [
-          ]
+          title: "Samsung",
+          type: [],
         },
         {
           name: "xiaomi",
-          title:"Xiaomi",
-          type: [
-          ]
-        }
-      ]
+          title: "Xiaomi",
+          type: [],
+        },
+      ],
     },
     {
       title: "Laptop - Pc",
@@ -100,166 +89,183 @@ export default function Product() {
       drop: [
         {
           name: "ha",
-          title:"ho",
-          type: [
-          ]
+          title: "ho",
+          type: [],
         },
         {
           name: "hi",
-          title:"ho",
-          type: [
-          ]
-        }
-      ]
+          title: "ho",
+          type: [],
+        },
+      ],
     },
     {
       title: "Phụ kiện",
       name: "gear",
       drop: [
         {
-          name:'',
-          type: [
-          ]
-        }
-      ]
+          name: "",
+          type: [],
+        },
+      ],
     },
-  ]
-  const [showDrop, setShowDrop] = useState('')
-  const [showDrop1, setShowDrop1] = useState('')
-  const [id, setId] = useState()
-  const [id1, setId1] = useState()
-  const [id2, setId2] = useState()
-  const onHandleShowDrop = (index) =>{
-    setShowDrop(index)
-    setId(index)
-    setId1()
-    setShowDrop1('')
-  }
-  const onHandleShowDrop1 = (index) =>{
-    setShowDrop1(index)
-    setId1(index)
-  }
-  const onHandleShowDrop2 = (index) =>{
-    setId2(index)
-  }
-  const onHandleDisShowDrop = () => {
-  }
+  ];
+  const [showDrop, setShowDrop] = useState("");
+  const [showDrop1, setShowDrop1] = useState("");
+  const [id, setId] = useState();
+  const [id1, setId1] = useState();
+  const [id2, setId2] = useState();
+  const onHandleShowDrop = (index) => {
+    setShowDrop(index);
+    setId(index);
+    setId1();
+    setShowDrop1("");
+  };
+  const onHandleShowDrop1 = (index) => {
+    setShowDrop1(index);
+    setId1(index);
+  };
+  const onHandleShowDrop2 = (index) => {
+    setId2(index);
+  };
+  const onHandleDisShowDrop = () => {};
   const onHandleDisShowDrop1 = (index) => {
-    setId2()
-    setShowDrop1(index)
-  }
+    setId2();
+    setShowDrop1(index);
+  };
   const onHandleDisShowDrop2 = () => {
-    setId2()
-  }
+    setId2();
+  };
   const onHandleDisShowDrop3 = () => {
-    setId()
-    setId1()
-    setId2()
-    setShowDrop('')
-    setShowDrop1('')
-  }
+    setId();
+    setId1();
+    setId2();
+    setShowDrop("");
+    setShowDrop1("");
+  };
   return (
     <div className="dropproduct">
-      <div 
-          className="dropproduct__drop"
-          // style={{width: `${listmenu[showDrop].drop[showDrop1].type && listmenu[showDrop].drop[showDrop1].type * 200}px`}}
-          onMouseLeave={() => onHandleDisShowDrop3()}
+      <div
+        className="dropproduct__drop"
+        // style={{width: `${listmenu[showDrop].drop[showDrop1].type && listmenu[showDrop].drop[showDrop1].type * 200}px`}}
+        onMouseLeave={() => onHandleDisShowDrop3()}
       >
-        <div style={{display: 'inline-grid', minWidth:'200px'}}>
-          {
-            listmenu && listmenu.map((item, index) => {
+        <div style={{ display: "inline-grid", minWidth: "200px" }}>
+          {listmenu &&
+            listmenu.map((item, index) => {
               return (
-                <Link className="btn dropproduct__drop__item  dropright"  to = {'/product/' + `${item.name}`}
-                      style={{ backgroundColor: `${id === index ? '#F8F4F4' :''}`, zIndex: '1'}}
-                      onMouseEnter={() => onHandleShowDrop(index)}
-                    onMouseLeave={() => onHandleDisShowDrop(index)}
+                <Link
+                  className="btn dropproduct__drop__item  dropright"
+                  to={"/product/" + `${item.name}`}
+                  style={{
+                    backgroundColor: `${id === index ? "#F8F4F4" : ""}`,
+                    zIndex: "1",
+                  }}
+                  onMouseEnter={() => onHandleShowDrop(index)}
+                  onMouseLeave={() => onHandleDisShowDrop(index)}
                 >
-                  <div 
-                    
-                  >
+                  <div>
                     <span>{item.title} </span>
-                    {
-                      item.drop.length > 1  ?  <p>&rsaquo;</p> : ''
-                    }
+                    {item.drop.length > 1 ? <p>&rsaquo;</p> : ""}
                   </div>
                 </Link>
-              )
-            })
-          }
+              );
+            })}
         </div>
         <div
-              style= {{  
-                backgroundColor:'white',
-                boxShadow: `${showDrop !=='' ?  '-1px 0px 3px #888888' : 'none'}`,
-                zIndex: '2'
-              }}
+          style={{
+            backgroundColor: "white",
+            boxShadow: `${showDrop !== "" ? "-1px 0px 3px #888888" : "none"}`,
+            zIndex: "2",
+          }}
         >
-          { 
-            showDrop !== '' ? (
-              listmenu[showDrop].drop &&
+          {showDrop !== ""
+            ? listmenu[showDrop].drop &&
               listmenu[showDrop].drop.map((item, index) => {
-                return(item.name !== '' ? (
-                    <Link to = {'/product/' + `${listmenu[showDrop].name}` + '/' + `${item.name}`}>
-                      <div  
-                          className="dropproduct__drop__right"
-                          style={{ backgroundColor: `${id1 === index ? '#F8F4F4' :''}`}}
-                          onMouseEnter={() => onHandleShowDrop1(index)}
-                          onMouseLeave={() => onHandleDisShowDrop1(index)}
-                          // onClick={() => onHandlerop1(index)}
-                      >
-                        <span>{item.title}</span>
-                        {
-                          listmenu[showDrop].drop[index].type.length > 1 ? <a>&rsaquo;</a> : ''
-                        }
-                      </div>
-                    </Link>
-                  ) : ''
-                )
+                return item.name !== "" ? (
+                  <Link
+                    to={
+                      "/product/" +
+                      `${listmenu[showDrop].name}` +
+                      "/" +
+                      `${item.name}`
+                    }
+                  >
+                    <div
+                      className="dropproduct__drop__right"
+                      style={{
+                        backgroundColor: `${id1 === index ? "#F8F4F4" : ""}`,
+                      }}
+                      onMouseEnter={() => onHandleShowDrop1(index)}
+                      onMouseLeave={() => onHandleDisShowDrop1(index)}
+                      // onClick={() => onHandlerop1(index)}
+                    >
+                      <span>{item.title}</span>
+                      {listmenu[showDrop].drop[index].type.length > 1 ? (
+                        <a>&rsaquo;</a>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+                  </Link>
+                ) : (
+                  ""
+                );
               })
-            ) : ''
-          }
+            : ""}
         </div>
         <div
-              style= {{ 
-                backgroundColor:'white',
-                boxShadow: `${showDrop1 !=='' ?  '-1px 0px 3px #888888' : 'none'}`,
-                zIndex: '3'
-              }}
+          style={{
+            backgroundColor: "white",
+            boxShadow: `${showDrop1 !== "" ? "-1px 0px 3px #888888" : "none"}`,
+            zIndex: "3",
+          }}
         >
-          {
-            showDrop1 !== '' && listmenu[showDrop].drop[showDrop1] ? (
-              listmenu[showDrop].drop[showDrop1].type  &&
+          {showDrop1 !== "" && listmenu[showDrop].drop[showDrop1]
+            ? listmenu[showDrop].drop[showDrop1].type &&
               listmenu[showDrop].drop[showDrop1].type.map((item, index) => {
-                return(item.name !== '' ? (
-                    <Link to = {'/product/' + `${listmenu[showDrop].name}` +
-                                '/' + `${listmenu[showDrop].drop[showDrop1].name}` + 
-                                '/' + `${item.ten}`}>
-                      <div  
-                          className="dropproduct__drop__right1"
-                          style={{ backgroundColor: `${id2 == index ? '#F8F4F4' :''}`}}
-                          onMouseEnter={() => onHandleShowDrop2(index)}
-                          onMouseLeave={() => onHandleDisShowDrop2()}
-                      >
-                          <span>{item.ten}</span>
-                      </div>
-                    </Link>
-                  ) : ''
-                )
+                return item.name !== "" ? (
+                  <Link
+                    to={
+                      "/product/" +
+                      `${listmenu[showDrop].name}` +
+                      "/" +
+                      `${listmenu[showDrop].drop[showDrop1].name}` +
+                      "/" +
+                      `${item.ten}`
+                    }
+                  >
+                    <div
+                      className="dropproduct__drop__right1"
+                      style={{
+                        backgroundColor: `${id2 == index ? "#F8F4F4" : ""}`,
+                      }}
+                      onMouseEnter={() => onHandleShowDrop2(index)}
+                      onMouseLeave={() => onHandleDisShowDrop2()}
+                    >
+                      <span>{item.ten}</span>
+                    </div>
+                  </Link>
+                ) : (
+                  ""
+                );
               })
-             ) :  ''
-          }
+            : ""}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-                  {/* <div className="product__dropright__item" onClick={() => onHandleDrop(index)}>
+{
+  /* <div className="product__dropright__item" onClick={() => onHandleDrop(index)}>
                     <span>{item.title} </span>
                     <a>&rsaquo;</a>
-                  </div> */}
-                  
-{/* <div className="product">
+                  </div> */
+}
+
+{
+  /* <div className="product">
         <div className="product__dropright btn-group dropright">
           <button 
                   // type="button" 
@@ -278,8 +284,10 @@ export default function Product() {
             <a className="dropdown-item" href="#">Separated link</a>
           </div>
         </div>
-    </div> */}
-{/* <div className='container__top__left '>
+    </div> */
+}
+{
+  /* <div className='container__top__left '>
         <ul>
             <li>
                 <div className="container__top__left__dropdown">
@@ -336,7 +344,8 @@ export default function Product() {
             <li>Laptop - Pc</li>
             <li>Phụ kiện</li> 
         </ul>
-    </div> */}
+    </div> */
+}
 // const [id, setId] =useState('')
 //   const [id2, setId2] =useState('')
 //   const [id3, setId3] =useState('')
@@ -360,35 +369,35 @@ export default function Product() {
 //   console.log(id);
 //   return (
 //     <div className="product">
-//         <div style={{ 
-//                       width: '200px', 
-//                       height: '300px', 
-//                       margin: '20px 200px', 
+//         <div style={{
+//                       width: '200px',
+//                       height: '300px',
+//                       margin: '20px 200px',
 //                       border: '1px solid black'
 //                     }}
-//         > 
+//         >
 //           <span style={{marginLeft: '75px', fontWeight: '800'}}>CASIO</span>
-//           <div style={{ 
+//           <div style={{
 //                         width: '180px',
 //                         height: '60px',
 //                         border: '1px solid black',
 //                         margin: '30px auto'
 //           }}>
 //             {
-              
+
 //               id && id === '1' ? (<><a>3</a></>) : ''
 //             }
-            
+
 //             {
-              
+
 //               id3 && id3 === '2' ? (<><a>x</a></>) : ''
 //             }
 //             {
-              
+
 //               id2 && id2 === '2' ? (<><a>7</a></>) : ''
 //             }
 //             {
-              
+
 //               id4 && id4 === '2' ? (<><a>=</a><a>28</a></>) : ''
 //             }
 //           </div>
---
+++ src/components/itemproduct/Itemproduct.js
@@ -0,0 +1,60 @@
+import "./Itemproduct.scss";
+import { Link } from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.css';
+
+const Itemproduct = ({ product }) => {
+  // console.log(product[0].type);
+  return (
+    <div className="card-group itemproduct">
+      {product &&
+        product.map((item, key) => {
+          return (
+            <>
+              {item.items.map((product, index) => {
+                // console.log(product);
+                return (
+                  <div className="col-3 itemproduct__box" key={index}>
+                    <div className="card text-center">
+                      <img
+                        className="card-img-top"
+                        src={product.image}
+                        alt="Card image cap"
+                      />
+                      <div className="card-body">
+                        <Link
+                          className="itemproduct__detail__title"
+                          to={`/purchase/${key + 1}/${product.id}/0/0`}
+                        >
+                          <h5 className="card-title">{product.title}</h5>
+                        </Link>
+                        <span className="card-text">{product.price}</span>
+                        {/* <button>Thêm vào giỏ hàng</button> */}
+                        <div style={{ margin: "10px 0px" }}>
+                          <Link to={`/purchase/${key + 1}/${product.id}/0/0`}>
+                            <p className="btn btn-primary col-6">Xem thêm</p>
+                          </Link>
+                          <Link to={`/compare/${key + 1}/${product.id}/0/0`}>
+                            <p className="btn btn-primary col-6">So sánh</p>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+            // <div className="itemproduct__detail">
+            //     <img className="image" src={product.image} alt={product.title} key={index}/>
+            //     <Link className='itemproduct__detail__title' to={`/productdetail/${index}`}>
+            //         <span>{product.title}</span>
+            //     </Link>
+            //     <span>{product.price}</span>
+            //     <button>Thêm vào giỏ hàng</button>
+            // </div>
+          );
+        })}
+    </div>
+  );
+};
+export default Itemproduct;
+//  onClick={() => handlePoductDetail(item.title, item.price, item.src)}
